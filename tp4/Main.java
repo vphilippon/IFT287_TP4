ca991480e7468677f47109383c5363e12abed38e
vphilippon <vincent.philippon@usherbrooke.ca>
2013-03-13 19:26:29 -0400

    Correction Date

--- tp4/Main.java
+++ tp4/Main.java
@@ -215,7 +215,8 @@ public class Main {
             String token = tokenizer.nextToken();
 
             try {
-                Date dt = (Date) FormatDate.convertirDate(token);
+                // TODO VALIDER SI OK APRES CORRECTION
+                Date dt = new Date(FormatDate.convertirDate(token).getTime());
                 return dt;
             } catch (ParseException e) {
                 throw new Tp4Exception("Date en format YYYY-MM-DD attendue à la place  de \"" +
