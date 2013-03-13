e7dcd690d7f5dceb047130e0ee52d7ebf1a36ed3
vphilippon <vincent.philippon@usherbrooke.ca>
2013-03-13 11:36:13 -0400

    Import TP3

---
+++ tp4/Tp4Exception.java
@@ -0,0 +1,10 @@
+package tp4;
+
+public class Tp4Exception extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public Tp4Exception(String message) {
+		super(message);
+	}
+}
