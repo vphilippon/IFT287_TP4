1f06a08a3411d2a9cf3a0cf40da5d9546202a8a4
Mathieu Larocque <math.larocque@gmail.com>
2013-03-13 21:16:47 -0400

    tupleFilm

--- tp4/TupleFilm.java
+++ tp4/TupleFilm.java
@@ -4,7 +4,8 @@ import java.sql.Date;
 
 class TupleFilm 
 {
-    
+    static private int courantID = 0;
+    private int ID;
     private String titre;
     private Date dateSortie;
     private String description;
@@ -12,30 +13,39 @@ class TupleFilm
     private String realisateur;
 
     public TupleFilm(String titre, Date dateSortie, String description, int duree, String realisateur) {
+        this.ID = ++courantID;
         this.titre = titre;
         this.dateSortie = dateSortie;
         this.description = description;
         this.duree = duree;
         this.realisateur = realisateur;
     }
+    
+    public int getCourantID(){
+        return courantID;
+    }
+    
+    public int getID(){
+        return this.ID;
+    }
 
     public Date getDateSortie() {
-        return dateSortie;
+        return this.dateSortie;
     }
 
     public String getDescription() {
-        return description;
+        return this.description;
     }
 
     public int getDuree() {
-        return duree;
+        return this.duree;
     }
 
     public String getRealisateur() {
-        return realisateur;
+        return this.realisateur;
     }
 
     public String getTitre() {
-        return titre;
+        return this.titre;
     }
 }
